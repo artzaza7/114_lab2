e1d8ddff2d7d4037dabff509bdf477aed109ff8d
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-08 17:05:08 +0700

    Lab2_2 ลองแก้ไขตามเงื่อนไขแล้วครับ

--- lab2_2.c
+++ lab2_2.c
@@ -6,14 +6,22 @@ int main()
     scanf("%c",&ans); fflush(stdin);
     scanf("%f",&tel);
     int new=tel;
-    printf("%d",new);
+    float money;
+    //printf("%d",new);
     if(ans=='A')
     {
-
+        money=200;
+        new=new-money;
+        new=new*3;
+        printf("%.2f",(money+new));
     }
     else
     {
-
+        money=400;
+        new=new-money;
+        money=300;
+        new=new*2;
+        printf("%.2f",(money+new));
     }
     return 0;
 }
