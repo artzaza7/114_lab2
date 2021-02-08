6f4aa383a37b6fd392fbaef02d2ea98908c60f10
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-08 17:36:41 +0700

    lab2_1 ลบเงื่อนไขออก

--- lab2_1.c
+++ lab2_1.c
@@ -6,24 +6,21 @@ int main()
     scanf("%d",&b);
     scanf("%d",&c); 
     int sum=a+b+c;
-    if(sum>=0&&sum<=100)
-    {
     if(sum>=80&&sum<=100)
     printf("A");
-    else if(sum>=75&&sum<=79)
+    else if(sum>=75)
     printf("B");
-    else if(sum>=70&&sum<=74)
+    else if(sum>=70)
     printf("B+");
-    else if(sum>=65&&sum<=69)
+    else if(sum>=65)
     printf("C");
-    else if(sum>=60&&sum<=64)
+    else if(sum>=60)
     printf("C+");
-    else if(sum>=55&&sum<=59)
+    else if(sum>=55)
     printf("D");
-    else if(sum>=50&&sum<=54)
+    else if(sum>=50)
     printf("D+");
-    else if(sum>=0&&sum<=49)
+    else 
     printf("F");
-    }
     return 0;
 }
