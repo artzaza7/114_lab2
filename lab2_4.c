7b0e2f750cdb5850ee4376ac815e897149a16e32
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-08 18:04:57 +0700

    lab2_4 ลอง check ครั้งที่ 1

---
+++ lab2_4.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+int main()
+{
+    char ans;
+    int number;
+    scanf("%c",&ans);
+    scanf("%d",&number);
+    if (ans>='A'&&ans<='I')
+    {
+        if(number%2!=0)
+        {
+            if(number>10)
+            {
+                if(number%10==5)
+                {
+                    printf("%c(^_^)/",92);
+                }
+                else 
+                printf("(^_^)");
+            }
+            else
+            {
+                printf("(^_^)");
+            }
+        }
+        else
+        {
+            printf("{@_@}");
+        }
+
+    }
+    else if (ans>='J'&&ans<='R')
+    {
+        if(number%2!=0)
+        {
+            if(number>10)
+            {
+                if(number%10==5)
+                {
+                    printf("%c(*o*)/",92);
+                }
+                else 
+                printf("(*o*)");
+            }
+            else
+            {
+                printf("(*o*)");
+            }
+        }
+        else
+        {
+            printf("{*v*}");
+        }
+    }
+    else
+    {
+        if(number%2!=0)
+        {
+            if(number>10)
+            {
+                if(number%10==5)
+                {
+                    printf("%c(T_T)/",92);
+                }
+                else 
+                printf("(T_T)");
+            }
+            else
+            {
+                printf("(T_T)");
+            }
+        }
+        else
+        {
+            printf("{x_x}");
+        }
+    }
+    return 0;
+}
