161b68dd22482f081cef7a34176fca24cc4f5983
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-08 16:59:55 +0700

    Lab2_2 ทดสอบรัน

---
+++ lab2_2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    float tel;
+    char ans;
+    scanf("%c",&ans); fflush(stdin);
+    scanf("%f",&tel);
+    int new=tel;
+    printf("%d",new);
+    if(ans=='A')
+    {
+
+    }
+    else
+    {
+
+    }
+    return 0;
+}
