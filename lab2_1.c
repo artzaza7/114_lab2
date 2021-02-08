33e1c7e4a8f3defb071150a7717a78eefc35216d
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-08 17:39:22 +0700

    lab2_1 แก้ไขเงื่อนไข

--- lab2_1.c
+++ lab2_1.c
@@ -8,17 +8,17 @@ int main()
     if(a+b+c>=80)
     printf("A");
     else if(a+b+c>=75)
-    printf("B");
-    else if(a+b+c>=70)
     printf("B+");
+    else if(a+b+c>=70)
+    printf("B");
     else if(a+b+c>=65)
-    printf("C");
-    else if(a+b+c>=60)
     printf("C+");
+    else if(a+b+c>=60)
+    printf("C");
     else if(a+b+c>=55)
+    printf("D๙");
+    else if(a+b+c> =50)
     printf("D");
-    else if(a+b+c>=50)
-    printf("D+");
     else 
     printf("F");
     return 0;
